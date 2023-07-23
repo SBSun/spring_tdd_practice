832a31c4e51a7b5a78716a22392351337380526d
SBSun <46879319+SBSun@users.noreply.github.com>
2023-07-23 21:17:53 +0900

    Feat : API 테스트로 전환 및 JPA 적용

--- product-order-service/src/main/java/example/productorderservice/product/ProductRepository.java
+++ product-order-service/src/main/java/example/productorderservice/product/ProductRepository.java
@@ -1,17 +1,7 @@
 package example.productorderservice.product;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
+interface ProductRepository extends JpaRepository<Product, Long> {
 
-@Repository
-class ProductRepository {
-    private Map<Long, Product> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
 }
